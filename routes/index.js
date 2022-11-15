f26d95f6682846916d1acfb2332d39545fde257f
r7qb <r7qb@fresnou.org>
2022-11-15 11:25:15 -0800

    commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', );
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+function getStory(formData) {
+  if()
+}
